d97c0cf8f116e3cf4e6e7dd7544985b045abc2e8
sailybaevvv <sailybaevvv@gmail.com>
2025-05-13 17:56:17 +0500

    Addition

--- db_config.py
+++ db_config.py
@@ -3,7 +3,8 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # Neon PostgreSQL connection string
-DATABASE_URL =
+DATABASE_URL = "postgresql://neondb_owner:npg_ATkY9HcUBS6L@ep-solitary-bush-a4ryhv8l-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require"
+
 # Create SQLAlchemy engine
 engine = create_engine(DATABASE_URL)
 
